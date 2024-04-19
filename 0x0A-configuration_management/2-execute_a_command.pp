af29302a9dabb3918e51331d990da7af6654ec2c
SammyObeezy <barasa07@gmail.com>
2024-04-19 05:10:43 -0700

    Add

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,9 @@
-# kill process killmenow
+# A manifest to kill a running process killmenow from the process table
+# on every puppet run
+exec { 'killing a process using pkill':
+  command  => 'pkill -9 killmenow',
+  path     => '/usr/bin:/bin',
+  onlyif   => 'pgrep killmenow',
+  provider => shell,
 
-exec { 'pkill pkill kilmenow':
-	path  => '/usr/bin/:usr/sbin/:bin',
 }
